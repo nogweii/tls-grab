c8fff51570cccd58910d93fbb983befc9093ce3c
Colin Shea <colin@evaryont.me>
2015-08-28 09:39:00 +0000

    remove debugging prints

--- tls-grab.go
+++ tls-grab.go
@@ -1,20 +1,12 @@
 package main
 
-import "fmt"
+//import "fmt"
 import "os"
-//import "net"
 import "crypto/tls"
-
 import "crypto/x509"
-//import "crypto/rsa"
-//import "crypto/dsa"
-//import "crypto/ecdsa"
-
 import "encoding/pem"
 
 func main() {
-  fmt.Println("google's public key:")
-
   conn, err := tls.Dial("tcp", "mail.google.com:443", &tls.Config{
     InsecureSkipVerify: true,
   })
@@ -24,7 +16,7 @@ func main() {
   conn.Close()
   tls_state := conn.ConnectionState()
   remote_certs := tls_state.PeerCertificates
-  for cert_count, cert := range remote_certs {
+  for _, cert := range remote_certs {
     if (cert.BasicConstraintsValid) {
       if (cert.IsCA) {
         continue
@@ -33,25 +25,17 @@ func main() {
       continue
     }
 
-    fmt.Println("got cert #", cert_count)
-
     var pem_type string = ""
 
     switch cert.PublicKeyAlgorithm {
     case x509.RSA:
-      fmt.Println("it's a RSA key")
       pem_type = "RSA PUBLIC KEY"
 
     case x509.ECDSA:
-      fmt.Println("it's a ECDSA key")
       pem_type = "ECDSA PUBLIC KEY"
 
     case x509.DSA:
-      fmt.Println("it's a DSA key")
       pem_type = "DSA PUBLIC KEY"
-
-    default:
-      fmt.Println("no clue")
     }
 
     pem_pubkey := &pem.Block{
@@ -60,5 +44,4 @@ func main() {
     }
     pem.Encode(os.Stdout, pem_pubkey)
   }
-  fmt.Println("didn't break?")
 }
