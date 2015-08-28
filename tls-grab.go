c8faed66eb277c526a19a8d50fa80142d198ee1b
Colin Shea <colin@evaryont.me>
2015-08-28 08:56:52 +0000

    and iterating through the list of keys

--- tls-grab.go
+++ tls-grab.go
@@ -4,8 +4,12 @@ import "fmt"
 //import "net"
 import "crypto/tls"
 
+import "crypto/x509"
+//import "crypto/rsa"
+//import "crypto/dsa"
+//import "crypto/ecdsa"
+
 func main() {
-  fmt.Println("public key goes where?")
   fmt.Println("google's public key:")
 
   conn, err := tls.Dial("tcp", "mail.google.com:443", &tls.Config{
@@ -17,8 +21,22 @@ func main() {
   conn.Close()
   tls_state := conn.ConnectionState()
   remote_certs := tls_state.PeerCertificates
-  for cert_count := range remote_certs {
+  for cert_count, cert := range remote_certs {
     fmt.Println("got cert #", cert_count)
+
+    switch cert.PublicKeyAlgorithm {
+    case x509.RSA:
+      fmt.Println("it's a RSA key")
+
+    case x509.ECDSA:
+      fmt.Println("it's a ECDSA key")
+
+    case x509.DSA:
+      fmt.Println("it's a DSA key")
+
+    default:
+      fmt.Println("no clue")
+    }
   }
   fmt.Println("didn't break?")
 }
